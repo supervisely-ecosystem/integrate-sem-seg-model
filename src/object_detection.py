c805c880f1d1a1c9ab1dfafa69c193521bcead2a
Arzamazov Nikita <arzamaznik@gmail.com>
2022-08-04 13:47:46 +0000

    functions in scripts + base notebook

--- src/object_detection.py
+++ src/object_detection.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+from tqdm import tqdm
 from dotenv import load_dotenv
 
 import supervisely as sly
@@ -16,75 +17,102 @@ load_dotenv(os.path.expanduser("~/supervisely.env"))
 # Initialize Supervisely
 api = sly.Api.from_env()
 project_id = int(os.environ["modal.state.slyProjectId"])
+src_project_info = api.project.get_info_by_id(project_id)
+workspace_id = src_project_info.workspace_id
+src_project_name = src_project_info.name
+dst_project_name = f"{src_project_name}_labeled_det"
 
-project_meta_json = api.project.get_meta(project_id)
+# Clone source project without existing annotations
+clone_task_id = api.project.clone_advanced(
+    project_id, workspace_id, dst_project_name, with_annotations=False
+)
+api.task.wait(clone_task_id, api.task.Status("finished"))
+dst_project_info = api.project.get_info_by_name(workspace_id, dst_project_name)
+
+# Get new project meta
+project_meta_json = api.project.get_meta(dst_project_info.id)
 project_meta = sly.ProjectMeta.from_json(project_meta_json)
 
 
-# Initialize Detectron2 model from config
-cfg = get_cfg()
-cfg.merge_from_file(
-    model_zoo.get_config_file(
-        "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
+def load_model():
+    # Initialize Detectron2 model from config
+    cfg = get_cfg()
+    cfg.merge_from_file(
+        model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")
     )
-)
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5 
-cfg.MODEL.DEVICE = "cpu"
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
-    "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
-)
-predictor = DefaultPredictor(cfg)
-class_names = MetadataCatalog.get(cfg.DATASETS.TRAIN[0]).get('thing_classes')
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+    cfg.MODEL.DEVICE = "cpu"
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
+        "COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"
+    )
+    predictor = DefaultPredictor(cfg)
+    class_names = MetadataCatalog.get(cfg.DATASETS.TRAIN[0]).get("thing_classes")
+    return predictor, class_names
 
 
-# Create meta from model classes and merge metas
-model_obj_classes = []
-for class_name in class_names:
-    model_obj_classes.append(sly.ObjClass(class_name, sly.Rectangle))
-confidence_tag_meta = sly.TagMeta(
-    name='confidence', 
-    value_type=sly.TagValueType.ANY_NUMBER, 
-    applicable_to=sly.TagApplicableTo.OBJECTS_ONLY
-)
-model_meta = sly.ProjectMeta(
-    obj_classes=model_obj_classes, 
-    tag_metas=sly.TagMetaCollection([confidence_tag_meta])
-)
-merged_meta = project_meta.merge(model_meta)
-api.project.update_meta(project_id, merged_meta)
-# Question: do we want to change source project? maybe clone?
+def get_model_classes(class_names):
+    # Create meta from model classes and merge metas
+    model_obj_classes = []
+    for class_name in class_names:
+        model_obj_classes.append(sly.ObjClass(class_name, sly.Rectangle))
+    confidence_tag_meta = sly.TagMeta(
+        name="confidence",
+        value_type=sly.TagValueType.ANY_NUMBER,
+        applicable_to=sly.TagApplicableTo.OBJECTS_ONLY,
+    )
+    model_meta = sly.ProjectMeta(
+        obj_classes=model_obj_classes, tag_metas=[confidence_tag_meta]
+    )
+    merged_meta = project_meta.merge(model_meta)
+    api.project.update_meta(dst_project_info.id, merged_meta)
+    return merged_meta
 
 
-datasets = api.dataset.get_list(project_id)
+def apply_model_to_image(image, model, class_names, project_meta):
+    img_size = image.shape[:2]
+    # Get predictions from Detectron2 model
+    outputs = model(image)
 
-# TODO: add progress
-for dataset_info in datasets:
-    ds_images = api.image.get_list(dataset_info.id)
-    for image_info in ds_images:
-        image = api.image.download_np(image_info.id)
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        img_size = image.shape[:2]
+    pred_classes = outputs["instances"].pred_classes.detach().numpy()
+    pred_class_names = [class_names[pred_class] for pred_class in pred_classes]
+    pred_scores = outputs["instances"].scores.detach().numpy()
+    pred_boxes = outputs["instances"].pred_boxes.tensor.detach().numpy()
 
-        # Get predictions from Detectron2 model
-        outputs = predictor(image)
+    # create Supervisely Labels from predicted objects
+    labels = []
+    for score, class_name, box in zip(pred_scores, pred_class_names, pred_boxes):
+        obj_class = project_meta.get_obj_class(class_name)
+        conf_tag = sly.Tag(
+            meta=project_meta.get_tag_meta("confidence"),
+            value=round(float(score), 4),
+        )
+        geometry = sly.Rectangle(top=box[1], left=box[0], bottom=box[3], right=box[2])
+        mask_label = sly.Label(geometry, obj_class, sly.TagCollection([conf_tag]))
+        labels.append(mask_label)
 
-        pred_classes = outputs["instances"].pred_classes.detach().numpy()
-        pred_class_names = [class_names[pred_class] for pred_class in pred_classes]
-        pred_scores = outputs["instances"].scores.detach().numpy()
-        pred_boxes = outputs["instances"].pred_boxes.tensor.detach().numpy()
+    # create Supervisely Annotation
+    annotation = sly.Annotation(img_size=img_size, labels=labels)
+    return annotation
 
-        # create Supervisely Labels from predicted objects
-        labels = []
-        for score, class_name, box in zip(pred_scores, pred_class_names, pred_boxes):
-            obj_class = merged_meta.get_obj_class(class_name)
-            conf_tag = sly.Tag(
-                meta=merged_meta.get_tag_meta('confidence'), 
-                value=round(float(score), 4)
-            )
-            geometry = sly.Rectangle(top=box[1], left=box[0], bottom=box[3], right=box[2])
-            mask_label = sly.Label(geometry, obj_class, sly.TagCollection([conf_tag]))
-            labels.append(mask_label)
 
-        # create and upload Supervisely Annotation
-        ann = sly.Annotation(img_size=img_size, labels=labels)
-        api.annotation.upload_ann(image_info.id, ann)
+def main():
+    model, class_names = load_model()
+    merged_project_meta = get_model_classes(class_names)
+
+    datasets = api.dataset.get_list(dst_project_info.id)
+    imgs_num = sum([dataset.images_count for dataset in datasets])
+    with tqdm(total=imgs_num) as pbar:
+        for dataset_info in datasets:
+            ds_images = api.image.get_list(dataset_info.id)
+            for image_info in ds_images:
+                image = api.image.download_np(image_info.id)
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                annotation = apply_model_to_image(
+                    image, model, class_names, merged_project_meta
+                )
+                api.annotation.upload_ann(image_info.id, annotation)
+                pbar.update()
+
+
+if __name__ == "__main__":
+    main()
