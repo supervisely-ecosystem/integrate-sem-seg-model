209882dbd4df357fac6d4e89a75b6207cb0e07fc
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-10-13 14:38:45 +0300

    headless serving (#3)
    * start UI
    
    * remove modal html
    
    * start menu
    
    * template not found
    
    * TODO: context_widgets -> singleton
    
    * widgets work
    
    * use device
    
    * cards done
    
    * container tests
    
    * todos
    
    * todo
    
    * todos
    
    * wip
    
    * load on device - done
    
    * selector - wip
    
    * menu-wip
    
    * TODO: use jinja to render menu (not v-for)
    
    * init serving layout in SDK
    
    * wip
    
    * literal fix
    
    * local env
    
    * upgrade sdk
    
    * black formatter
    
    * annotations
    
    * wip
    
    * refresh gitignore
    
    * file - work in progress
    
    * wip
    
    * wip
    
    * download from team files - wip
    
    * colors done
    
    * wip
    
    * clear old files
    
    Co-authored-by: Maxim Kolomeychenko <maxim@supervise.ly>

--- src/main.py
+++ src/main.py
@@ -1,18 +1,16 @@
-from distutils.log import warn
 import os
-from typing import Literal, List
+from typing_extensions import Literal
+from typing import List
 import cv2
 import json
 from dotenv import load_dotenv
+import torch
 import supervisely as sly
-from typing_extensions import Literal
 
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.data import MetadataCatalog
-
-from supervisely.app.widgets import card
 
 load_dotenv("local.env")
 load_dotenv(os.path.expanduser("~/supervisely.env"))
@@ -28,7 +26,7 @@ class MyModel(sly.nn.inference.InstanceSegmentation):
         device: Literal["cpu", "cuda", "cuda:0", "cuda:1", "cuda:2", "cuda:3"] = "cpu",
     ):
         ####### CODE FOR DETECTRON2 MODEL STARTS #######
-        with open(os.path.join(model_dir, "model_info.json"), "r") as myfile:
+        with open(os.path.join(self.model_dir, "model_info.json"), "r") as myfile:
             model_info = json.loads(myfile.read())
         cfg = get_cfg()
         cfg.merge_from_file(
@@ -36,54 +34,56 @@ class MyModel(sly.nn.inference.InstanceSegmentation):
             model_zoo.get_config_file(model_info["architecture"])
         )
         cfg.MODEL.DEVICE = device  # learn more in torch.device
-        cfg.MODEL.WEIGHTS = os.path.join(model_dir, "model_weights.pkl")
+        cfg.MODEL.WEIGHTS = os.path.join(self.model_dir, "model_weights.pkl")
 
         self.predictor = DefaultPredictor(cfg)
-        self.class_names = MetadataCatalog.get(cfg.DATASETS.TRAIN[0]).get(
-            "thing_classes"
-        )
+        self.class_names = MetadataCatalog.get(cfg.DATASETS.TRAIN[0]).get("thing_classes")
         ####### CODE FOR DETECTRON2 MODEL ENDS #########
-        print(f"✅ Model has been successfully loaded on {device} device")
 
-    def get_classes(self) -> list[str]:
-        return self.class_names  # ["cat", "dog", ...]
+        print(f"✅ Model has been successfully loaded on {device.upper()} device")
+
+    def get_classes(self) -> List[str]:
+        return self.class_names  # e.g. ["cat", "dog", ...]
 
     def predict(
         self, image_path: str, confidence_threshold: float = 0.8
-    ) -> list[sly.nn.PredictionMask]:
+    ) -> List[sly.nn.PredictionMask]:
         image = cv2.imread(image_path)  # BGR
 
-        ####### CODE FOR DETECTRON2 MODEL STARTS #######
+        ####### CUSTOM CODE FOR MY MODEL STARTS (e.g. DETECTRON2) #######
         outputs = self.predictor(image)  # get predictions from Detectron2 model
         pred_classes = outputs["instances"].pred_classes.detach().numpy()
         pred_class_names = [self.class_names[pred_class] for pred_class in pred_classes]
         pred_scores = outputs["instances"].scores.detach().numpy().tolist()
         pred_masks = outputs["instances"].pred_masks.detach().numpy()
-        ####### CODE FOR DETECTRON2 MODEL ENDS #########
+        ####### CUSTOM CODE FOR MY MODEL ENDS (e.g. DETECTRON2)  ########
 
         results = []
         for score, class_name, mask in zip(pred_scores, pred_class_names, pred_masks):
+            # filter predictions by confidence
             if score >= confidence_threshold:
                 results.append(sly.nn.PredictionMask(class_name, mask, score))
         return results
 
 
-team_id = int(os.environ["context.teamId"])
-model_dir = os.path.abspath(os.environ["context.slyFolder"])
-device = os.environ.get("modal.state.device", "cpu")  # @TODO: reimplement
+model_dir = sly.env.folder()
+print("Model directory:", model_dir)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("Using device:", device)
 
 m = MyModel(model_dir)
+m.load_on_device(device)
 
 if sly.is_production():
-    # code below is running on Supervisely platform in production
-    # just ignore it during development and testing
+    # this code block is running on Supervisely platform in production
+    # just ignore it during development
     m.serve()
 else:
     # for local development and debugging
-    m.load_on_device("cpu")
     image_path = "./demo_data/image_01.jpg"
     confidence_threshold = 0.7
     results = m.predict(image_path, confidence_threshold)
     vis_path = "./demo_data/image_01_prediction.jpg"
     m.visualize(results, image_path, vis_path)
-    print("predictions and visualization have been created")
+    print(f"predictions and visualization have been saved: {vis_path}")
