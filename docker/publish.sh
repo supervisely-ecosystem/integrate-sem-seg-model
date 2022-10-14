9da9ca344338d68adac3259dcc8bbdde00bfa466
Maxim Kolomeychenko <maxim@supervise.ly>
2022-10-14 11:54:19 +0300

    dockerimage 1.0.2

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/detectron2-demo:1.0.1 . && \
-docker push supervisely/detectron2-demo:1.0.1
+docker build -t supervisely/detectron2-demo:1.0.2 . && \
+docker push supervisely/detectron2-demo:1.0.2
